9d60f0eedf311226cd412c80c7370decfe02f4be
Aarón Huamán <aaron20hc@gmail.com>
2020-06-28 02:07:36 -0500

    Ultimate Commit 2

--- src/main/java/com/app/BfoodApplication.java
+++ src/main/java/com/app/BfoodApplication.java
@@ -22,7 +22,7 @@ public class BfoodApplication {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                         registry.addMapping("/**")
-                                .allowedOrigins("http://localhost:3000") //localhost:3000
+                                .allowedOrigins("https://bfood.netlify.app/") //localhost:3000
                                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                                 .maxAge(3600);
                 }
